5ef7e4a4a4fde8eb9863a95dddeb673d06019c34
sist <sist@hj>
2018-04-19 17:56:47 +0900

    혜지 공지사항 작업중 디자인등등

---
+++ src/main/webapp/sql/board2.sql
@@ -0,0 +1,28 @@
+  drop table board2
+
+create table board2(
+
+ num int PRIMARY KEY ,
+ writer VARCHAR2(100) ,
+  subject varchar2(100),
+  content varchar2(3000),
+
+  ref int not null,
+  re_step int not null,
+  re_level int not null,
+
+  regdate date not null,
+  readcount int,
+  filename VARCHAR2(100) ,
+  filesize INT
+
+
+)
+select * from  board2
+
+drop sequence board2Ser
+create sequence board2Ser
+  start with 1
+  increment by 1
+  NOMAXVALUE
+  NOCYCLE ;
